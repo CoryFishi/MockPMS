e3f2b0878030d49d4ff4e62a3177d1ffcfc045d0
Cory Fishburn <coryjr2002@gmail.com>
2024-11-12 08:02:11 -0700

    Update call to action fixes

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
 import { supabase } from "../supabaseClient";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { IoWarning } from "react-icons/io5";
 
 const AuthContext = createContext();
 
@@ -27,6 +30,48 @@ export const AuthProvider = ({ children }) => {
       return null;
     }
     setTokens(data?.tokens || []);
+
+    if (data?.tokens < 1 && user) {
+      toast.custom(
+        (t) => (
+          <div className="max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5">
+            <div className="flex-1 w-0 p-4">
+              <div
+                className="flex items-center hover:cursor-pointer"
+                onClick={() => {
+                  toast.dismiss(t.id);
+                  window.location.href =
+                    "https://propertymanager-dev.netlify.app/settings";
+                }}
+              >
+                <div className="flex-shrink-0 flex items-center justify-center">
+                  <IoWarning className="text-4xl" />
+                </div>
+                <div className="ml-3 flex-1">
+                  <p className="text-sm font-medium text-gray-900">
+                    Management Dashboard
+                  </p>
+                  <p className="mt-1 text-sm text-gray-500">
+                    Looks like you have yet to authenticate any facilities.
+                    Click <span className="text-blue-500">here</span> to
+                    authenticate a facility.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        ),
+        { duration: 30000 }
+      );
+    }
   };
 
   // Get all favorited tokens
