812721d85558ced51cfebca9c047bd74e607ad90
Cory Fishburn <coryjr2002@gmail.com>
2024-12-23 13:55:14 -0700

    supabase server side

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Public Client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL;
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_KEY || process.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // Admin Client
-const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
+const supabaseServiceKey =
+  import.meta.env.VITE_SUPABASE_SERVICE_KEY ||
+  process.env.VITE_SUPABASE_SERVICE_KEY;
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
