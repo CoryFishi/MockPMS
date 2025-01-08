2b089f0972b2b9955480c18a2f600328beeb2ba2
Cory Fishburn <coryjr2002@gmail.com>
2025-01-08 12:09:28 -0700

    html changes

--- src/functions/facilityReportEmail.jsx
+++ src/functions/facilityReportEmail.jsx
@@ -14,136 +14,94 @@ export async function sendFacilityReportEmail(
   currentWeather
 ) {
   const html = ReactDOMServer.renderToString(
-    <tr className="hover:bg-gray-100 dark:hover:bg-darkNavSecondary relative border border-gray-300 dark:border-border">
-      <td className="px-4 py-2">
-        <div className="flex items-center gap-2 cursor-pointer">
-          <button className="text-blue-500">+</button>
-          {facility.name}
-          <FaExternalLinkAlt
-            className="text-blue-300 group-hover:text-blue-500"
-            title={
-              facility.environment === "cia-stg-1.aws."
-                ? `https://portal.${facility.environment}insomniaccia.com/facility/${facility.id}/dashboard`
-                : `https://portal.insomniaccia${facility.environment}.com/facility/${facility.id}/dashboard`
-            }
-            onClick={(e) => {
-              e.stopPropagation();
-              e.prev;
-              const baseUrl =
+    <table
+      style={{
+        width: "100%",
+        borderCollapse: "collapse",
+        fontFamily: "Arial, sans-serif",
+        fontSize: "14px",
+        color: "#333",
+      }}
+    >
+      <thead>
+        <tr style={{ backgroundColor: "#f4f4f4" }}>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Facility Name
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Edge Router
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Online APs
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Offline APs
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Okay Status
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Warning Status
+          </th>
+          <th style={{ border: "1px solid #ccc", padding: "8px" }}>
+            Error Status
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            <a
+              href={
                 facility.environment === "cia-stg-1.aws."
                   ? `https://portal.${facility.environment}insomniaccia.com/facility/${facility.id}/dashboard`
-                  : `https://portal.insomniaccia${facility.environment}.com/facility/${facility.id}/dashboard`;
-              window.open(baseUrl, "_blank");
-            }}
-          />
-        </div>
-      </td>
-      <td className="px-4 py-2" title={edgeRouter?.connectionStatusMessage}>
-        <div className="inline-flex items-center gap-1">
-          {edgeRouter?.connectionStatus === "error" ? (
-            <IoIosWarning className="text-red-500 mr-2" />
-          ) : edgeRouter?.connectionStatus === "warning" ? (
-            <IoIosWarning className="text-yellow-500 mr-2" />
-          ) : (
-            <FaCheckCircle className="text-green-500 mr-2" />
-          )}
-          {edgeRouter?.name}
-        </div>
-      </td>
-      <td
-        className="px-4 py-2"
-        title={
-          Array.isArray(accessPoints)
-            ? `${accessPoints
-                .filter((ap) => ap.isDeviceOffline === false)
-                .map((ap) => ap.name)
-                .join(", ")}\n${Math.round(
-                (accessPoints.filter((ap) => ap.isDeviceOffline === false)
-                  .length /
-                  accessPoints.length) *
-                  100
-              )}% Online`
-            : ""
-        }
-      >
-        {Array.isArray(accessPoints)
-          ? accessPoints.filter((ap) => ap.isDeviceOffline === false).length
-          : 0}
-      </td>
-
-      <td
-        className="px-4 py-2"
-        title={
-          Array.isArray(accessPoints)
-            ? `${accessPoints
-                .filter((ap) => ap.isDeviceOffline === true)
-                .map((ap) => ap.name)
-                .join(", ")}\n${Math.round(
-                (accessPoints.filter((ap) => ap.isDeviceOffline === true)
-                  .length /
-                  accessPoints.length) *
-                  100
-              )}% Offline`
-            : ""
-        }
-      >
-        {Array.isArray(accessPoints)
-          ? accessPoints.filter((ap) => ap.isDeviceOffline === true).length
-          : 0}
-      </td>
-
-      <td
-        className="px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-600 hover:cursor-pointer"
-        onClick={() => openSmartLockModal("good")}
-        title={
-          Math.round(
-            (smartlockSummary?.okCount /
-              (smartlockSummary?.okCount +
-                smartlockSummary?.warningCount +
-                smartlockSummary?.errorCount)) *
-              100
-          ) +
-          "%" +
-          " Okay Status"
-        }
-      >
-        {smartlockSummary?.okCount}
-      </td>
-      <td
-        className="px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-600 hover:cursor-pointer"
-        onClick={() => openSmartLockModal("warning")}
-        title={
-          Math.round(
-            (smartlockSummary?.warningCount /
-              (smartlockSummary?.okCount +
-                smartlockSummary?.warningCount +
-                smartlockSummary?.errorCount)) *
-              100
-          ) +
-          "%" +
-          " Warning Status"
-        }
-      >
-        {smartlockSummary?.warningCount}
-      </td>
-      <td
-        className="px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-600 hover:cursor-pointer"
-        onClick={() => openSmartLockModal("error")}
-        title={
-          Math.round(
-            (smartlockSummary?.errorCount /
-              (smartlockSummary?.okCount +
-                smartlockSummary?.warningCount +
-                smartlockSummary?.errorCount)) *
-              100
-          ) +
-          "%" +
-          " Error Status"
-        }
-      >
-        {smartlockSummary?.errorCount}
-      </td>
-    </tr>
+                  : `https://portal.insomniaccia${facility.environment}.com/facility/${facility.id}/dashboard`
+              }
+              style={{ color: "#007bff", textDecoration: "none" }}
+              target="_blank"
+            >
+              {facility.name}
+            </a>
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            <span
+              style={{
+                display: "inline-block",
+                marginRight: "8px",
+                color:
+                  edgeRouter?.connectionStatus === "error"
+                    ? "red"
+                    : edgeRouter?.connectionStatus === "warning"
+                    ? "orange"
+                    : "green",
+              }}
+            >
+              &#9679;
+            </span>
+            {edgeRouter?.name}
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            {Array.isArray(accessPoints)
+              ? accessPoints.filter((ap) => !ap.isDeviceOffline).length
+              : 0}
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            {Array.isArray(accessPoints)
+              ? accessPoints.filter((ap) => ap.isDeviceOffline).length
+              : 0}
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            {smartlockSummary?.okCount}
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            {smartlockSummary?.warningCount}
+          </td>
+          <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+            {smartlockSummary?.errorCount}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 
   try {
